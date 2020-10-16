8be73af60021dcefa711b24df40123b57b8cf75e
Henry <Henrychaconrm@gmail.com>
2020-10-15 22:58:45 -0600

    Primer commit para subir repositorio

---
+++ src/app/models/empleado.ts
@@ -0,0 +1,10 @@
+// mapeo de los datos
+
+export class Empleado {
+    nombreCompleto: string;
+    telefono: number;
+    correo: string;
+    fechaIngreso: Date;
+    estadoCivil: string;
+    sexo: string;
+  }
